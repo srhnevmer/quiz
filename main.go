3f2747c1196220e9f5713dea6661ec6ad68f149e
Serhii Nevmer <srhnevmer@proton.me>
2025-04-09 17:39:33 +0300

    feat: add a timer Add the ability to run a timer as a background process

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"encoding/csv"
 	"flag"
 	"fmt"
@@ -12,14 +13,20 @@ import (
 	"runtime"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
 	fileName := flag.String("n", "problems", "set the name of the file with the problems")
 	reorder := flag.Bool("r", false, "change the order of problems")
+	timer := flag.Int64("t", 30, "time limit to complete the quiz")
 	flag.Parse()
 
-	q := quiz{fileName: *fileName, reorder: *reorder}
+	q := quiz{
+		fileName:  *fileName,
+		reorder:   *reorder,
+		timeLimit: time.Duration(*timer),
+	}
 	q.makeProblemList()
 	q.run()
 }
@@ -34,6 +41,7 @@ type quiz struct {
 	reorder       bool
 	numCorrectAns uint8
 	problems      []problem
+	timeLimit     time.Duration
 }
 
 func (q *quiz) makeProblemList() {
@@ -77,11 +85,30 @@ func formatData(data [][]string) []problem {
 }
 
 func (q *quiz) run() {
+Loop:
+	for {
+		clear()
+		fmt.Fprintf(os.Stdout, "This quiz has a time limit\nYou have %d seconds to complete this quiz\nPress [y]es to continue or [n]o to exit: ", q.timeLimit)
+		switch in := userIn(); {
+		case bytes.Equal(in, []byte{121}), bytes.Equal(in, []byte{121, 101, 115}):
+			break Loop
+		case bytes.Equal(in, []byte{110}), bytes.Equal(in, []byte{110, 111}):
+			os.Exit(0)
+		}
+	}
+
+	timer := time.NewTimer(q.timeLimit * time.Second)
+	go func() {
+		<-timer.C
+		resultNotification(q.numCorrectAns, len(q.problems))
+		os.Exit(0)
+	}()
+
 	for i := range q.problems {
 		clear()
 		q.print(i)
 		a, _ := strconv.Atoi(q.problems[i].answer)
-		ua, err := strconv.Atoi(handleUserAns())
+		ua, err := strconv.Atoi(string(userIn()))
 		if err != nil {
 			continue
 		}
@@ -93,23 +120,13 @@ func (q *quiz) run() {
 		q.numCorrectAns++
 	}
 
-	fmt.Fprintf(os.Stdout, "Number of correct answers: %d\nTotal number of problems: %d\n", q.numCorrectAns, len(q.problems))
+	resultNotification(q.numCorrectAns, len(q.problems))
 }
 
 func (q quiz) print(idx int) {
 	var str strings.Builder
 	str.WriteString(fmt.Sprintf("Problem: %d\nQ: %s\nA: ", idx+1, q.problems[idx].question))
 	fmt.Fprint(os.Stdout, str.String())
-}
-
-func handleUserAns() string {
-	s := bufio.NewScanner(os.Stdin)
-	s.Scan()
-	err := s.Err()
-	if err != nil {
-		log.Fatal(err)
-	}
-	return strings.TrimSpace(s.Text())
 }
 
 func clear() {
@@ -124,4 +141,19 @@ func clear() {
 		cmd.Stdout = os.Stdout
 		cmd.Run()
 	}
+}
+
+func userIn() []byte {
+	s := bufio.NewScanner(os.Stdin)
+	s.Scan()
+	err := s.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return bytes.ToLower(bytes.TrimSpace(s.Bytes()))
+}
+
+func resultNotification(quantity uint8, total int) {
+	clear()
+	fmt.Fprintf(os.Stdout, "Number of correct answers: %d\nTotal number of problems: %d\n", quantity, total)
 }
